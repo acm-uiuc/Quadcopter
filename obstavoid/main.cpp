767c424d5ae4ca715b43312e9b731f955a1c5456
Nikoli Dryden <ndryden@xenosgame.com>
2013-04-29 21:46:54 -0500

    Updated main to accept as input two images.

--- obstavoid/main.cpp
+++ obstavoid/main.cpp
@@ -3,7 +3,12 @@
 #include "obstavoid.h"
 
 int main(int argc, char** argv) {
-    cv::Mat m;
-    match_pipeline(m, m);
+    if (argc < 3) {
+	printf("Need two images to test.\n");
+	return 1;
+    }
+    cv::Mat img1 = cv::imread(argv[1]);
+    cv::Mat img2 = cv::imread(argv[2]);
+    match_pipeline(img1, img2);
     return 0;
 }
