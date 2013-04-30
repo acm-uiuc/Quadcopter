e207ef07d5406880ee6dc7e058c52119bcc244cd
Nikoli Dryden <ndryden@xenosgame.com>
2013-04-30 09:43:44 -0500

    Added report skeleton using SIGGraph template.

---
+++ obstavoid/report/auto/report.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "report"
+ (lambda ()
+    (TeX-run-style-hooks
+     "caption"
+     "textfont=it"
+     "labelfont=bf"
+     "fixltx2e"
+     "parskip"
+     "graphicx"
+     "times"
+     "helvet"
+     "scaled=.92"
+     "latex2e"
+     "acmsiggraph10"
+     "acmsiggraph"
+     "")))
+
