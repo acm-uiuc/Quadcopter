3fcf5c25b6aa0a367dd69ef5ec9aaf91b9abe6f8
Nikoli Dryden <ndryden@xenosgame.com>
2013-05-03 10:50:48 -0500

    Added basic presentation template. Updated gitignore.

---
+++ obstavoid/pres/auto/pres.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "pres"
+ (lambda ()
+    (TeX-run-style-hooks
+     "palatino"
+     "cancel"
+     "graphicx"
+     ""
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "t"
+     "compress"
+     "xcolor=x11names")))
+
